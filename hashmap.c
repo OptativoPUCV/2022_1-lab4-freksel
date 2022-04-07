210d162861dd34e6caa7f7de611f7d8a0d4e5a93
ALEJANDROMARI10 <>
2022-04-07 16:02:44 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -79,8 +79,8 @@ void eraseMap(HashMap *map, char *key) {
 
 Pair *searchMap(HashMap *map, char *key) {
   long index2= hash(key,map->capacity);
-  while(map->buckets[index2]!=NULL && (is_equal(map->buckets[index]->key,key)!=1)){
-    index=(index+1) % map->capacity;
+  while(map->buckets[index2]!=NULL && (is_equal(map->buckets[index2]->key,key)!=1)){
+    index2=(index2+1) % map->capacity;
     }
   if(is_equal(map->buckets[index2]->key,key)==1){
     return map->buckets[index2];
