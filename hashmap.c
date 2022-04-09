3f6fcff3b767fc4d4a2a9903775fc5cbc5e680b6
ALEJANDROMARI10 <>
2022-04-09 20:53:09 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -103,7 +103,12 @@ Pair *searchMap(HashMap *map, char *key) {
 }
 
 Pair *firstMap(HashMap *map) {
-	return NULL;
+  int i=0;
+  while(map->buckets[i]==NULL){
+    i++;
+  }
+  map->current=i;
+	return map->current;
 }
 
 Pair *nextMap(HashMap *map) {
