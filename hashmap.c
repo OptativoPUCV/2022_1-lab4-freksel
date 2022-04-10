b177c73fd5075862b6661dcea32f9f7776398683
ALEJANDROMARI10 <>
2022-04-10 23:57:22 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -59,6 +59,20 @@ void insertMap(HashMap *map, char *key, void *value) {
 }
 
 void enlarge(HashMap *map) {
+  int Mo;
+  int i=0;
+  char* M1[100];
+  Pair** aux=map->buckets;
+  Mo=map->capacity*2;
+  HashMap *m2 = (HashMap*)malloc(sizeof(HashMap));
+  m2->size=0;
+  m2->capacity=Mo;
+  m2->current=-1;
+  
+  while(i<map->capacity/2){
+    insertMap(m2,(map->buckets[i]->key),aux);
+    i++;
+    }
 	enlarge_called = 1; // no borrar (testing purposes)
 }
 
