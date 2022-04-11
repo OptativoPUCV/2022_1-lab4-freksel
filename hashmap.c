24f6a5b74a2f96a605a7d11578d954e14b5a72f9
ALEJANDROMARI10 <>
2022-04-11 02:36:07 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -68,7 +68,7 @@ void enlarge(HashMap *map) {
   m2->capacity=Mo;
   m2->current=-1;
   m2->buckets =(Pair **)calloc(Mo, sizeof(Pair *));
-  while(i<map->capacity){
+  while(i!=map->capacity){
     if(map->buckets[i]!=NULL && map->buckets[i]->key!=NULL){
       insertMap(m2,(map->buckets[i]->key),aux[i]->value);
       m2->size++;
